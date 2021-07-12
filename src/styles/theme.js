4b3e43daa92bc7663b223091b08b45c12b0f7486
João Pedro <joaomljcanal@gmail.com>
2021-07-12 09:38:55 -0300

    Commit Inicial

---
+++ src/styles/theme.js
@@ -0,0 +1,43 @@
+export default {
+  grid: {
+    container: '111rem',
+  },
+  colors: {
+    backgroundSecondary: '#F1F9FE',
+    backgroundDefault: '#D9E6F6',
+    primaryText: '#2E7BB4',
+    secondaryText: '#388BB0',
+    tertiaryText: '#2F4A71',
+    primaryElement: '#6F92BB',
+    anotherElement: '#5579A1',
+    gray1: '#333333',
+    gray2: '#5A5A5A',
+    gray3: '#999999',
+    gray4: '#C5C6CA',
+    gray5: '#F4F4F4',
+  },
+  font: {
+    family:
+      "Rubik, -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif",
+    normal: 400,
+    bold: 600,
+    sizes: {
+      xsmall: '1.2rem',
+      small: '1.4rem',
+      medium: '1.6rem',
+      large: '1.8rem',
+      xlarge: '2.0rem',
+      xxlarge: '2.8rem',
+      huge: '5.2rem',
+    },
+  },
+  spacings: {
+    xxsmall: '0.8rem',
+    xsmall: '1rem',
+    small: '1.2rem',
+    medium: '1.4rem',
+    large: '1.6rem',
+    xlarge: '2rem',
+    xxlarge: '2.4rem',
+  },
+};
